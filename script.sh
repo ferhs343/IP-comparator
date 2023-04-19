f3e05cfbdc5fe77bc499d2516370aa96e0226b83
ferhs343 <114626248+ferhs343@users.noreply.github.com>
2023-04-19 13:59:03 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# ELABORADO POR ==> LUIS HERRERA (12 - 18 ABRIL 2023)
-# ULTIMA MODIFICACION ==> 18 ABRIL 2023
+# ELABORADO POR ==> LUIS HERRERA (12 - 19 ABRIL 2023)
+# ULTIMA MODIFICACION ==> 19 ABRIL 2023
 # XD
 
 clear
@@ -10,7 +10,7 @@ clear
 files=(
         'libra-liberty'
         'escorpio-liberty'
-        'virgo-liberty'
+        'virgus-liberty'
         'capricornio-liberty'
 )
 
@@ -33,48 +33,61 @@ green="\e[0;32m\033[1m"
 yellow="\e[0;33m\033[1m"
 gray="\e[0;37m\033[2m"
 
+#BANNER
+function banner() {
+        echo -e " ${green}"
+        echo -e " ----------  --------      ------------  -----------  ---       ----  ----------"
+        echo -e " |__    __| |        |            |   _______|  |         |  |   \    /   |  |        |"
+        echo -e "    |  |    |        |  __ |   |         |   ___   |  |    \__/    |  |        |"
+        echo -e "    |  |    |   -----      |   |         |  |___|  |  |            |  |  -------"
+        echo -e "    |  |    |  |          |   |         |         |  |  |\ __/|   |  |  |      "
+        echo -e " ---   ---- |  |           |   |______   |         |  |  |     |   |  |  |      "
+        echo -e " |________| |__|           |__________|  |_________|  |__|     | __|  |__|         V 1.0"
+        echo -e "                                                                                   by Luis Herrera xD ${white}"
+}
+
 #FUNCTION TO GET MATCHED IPv4 SEGMENTS
 function matched() {
 
         for (( i=0;i<=$n_elements-1;i++ ))
         do
                 echo " "
-                echo -e "${yellow}----------------------------------------------------------------------${white}"
-                echo -e "${yellow} LISTING MATCHING IPv4 SEGMENTS WITH FILE ${files[$i]}${extention}${white}"
-                echo -e "${yellow}----------------------------------------------------------------------${white}"
+                echo -e "${yellow} ----------------------------------------------------------------------${white}"
+                echo -e "${yellow}  LISTING MATCHING IPv4 SEGMENTS WITH FILE ${files[$i]}${extention}${white}"
+                echo -e "${yellow} ----------------------------------------------------------------------${white}"
                 echo " "
 
                 for j in $(cat ${file})
                 do
-			flag2=1
+                        flag2=1
                         for k in  $(cat "${files[$i]}"${extention})
                         do
-				if [ "$j" == "$k" ];
-				then
-					flag2=0
-					break
+                                if [ "$j" == "$k" ];
+                                then
+                                        flag2=0
+                                        break
                                fi
                         done
 
-			if [ "$file" != "${files[$i]}"$extention ];
+                        if [ "$file" != "${files[$i]}"$extention ];
                         then
-				if [ "$flag2" -eq 0 ];
-				then
-					echo -e "${green}[+] " ${j}${white}
-                                	sleep 0.1
-                               		total=$((total+1))
-			 	fi
-			fi
+                                if [ "$flag2" -eq 0 ];
+                                then
+                                        echo -e "${green} [+] " ${j}${white}
+                                        sleep 0.1
+                                        total=$((total+1))
+                                fi
+                        fi
                  done
 
                  if [ "$file" != "${files[$i]}"$extention ];
                  then
-			echo " "
-                        echo -e "${green}[+] ${total} Matched Ipv4 segments."
+                        echo " "
+                        echo -e "${green} [+] ${total} Matched Ipv4 segments."
 
                  elif [ "$file" == "${files[$i]}"$extention ];
                  then
-                        echo -e "${green}[+] OMITTED${white}"
+                        echo -e "${red} [+] OMITTED${white}"
                  fi
 
                  sleep 1
@@ -88,17 +101,17 @@ function uniqs() {
 
         for  (( i=0;i<=$n_elements-1;i++ ))
         do
-		if [[ -f ./"${new_directory}/new-${files[$i]}-${code_file}${extention2}" ]];
-	        then
-        	        code_file=$((code_file+1))
-       		fi
+                while [[ -f ./"${new_directory}/new-${files[$i]}-${code_file}${extention2}" ]];
+                do
+                        code_file=$((code_file+1))
+                done
 
                 echo " "
-                echo -e "${yellow}----------------------------------------------------------------------${white}"
-                echo -e "${yellow} UNIQUE IPv4 SEGMENTS WITH FILE ${files[$i]}$extention${white}"
-                echo -e "${yellow}----------------------------------------------------------------------${white}"
+                echo -e "${yellow} ----------------------------------------------------------------------${white}"
+                echo -e "${yellow}  UNIQUE IPv4 SEGMENTS WITH FILE ${files[$i]}$extention${white}"
+                echo -e "${yellow} ----------------------------------------------------------------------${white}"
                 echo " "
-		echo -e "${green}[+] Wait....... ${white}"
+                echo -e "${green} [+] Wait....... ${white}"
 
                 for j in $(cat ${file})
                 do
@@ -116,26 +129,32 @@ function uniqs() {
                         then
                                 if [ "$flag2" -eq 1 ];
                                 then
-					echo $j >> "${new_directory}/new-${files[$i]}-${code_file}${extention2}"
-					sleep 0.2
+                                        echo $j >> "${new_directory}/new-${files[$i]}-${code_file}${extention2}"
+                                        sleep 0.2
                                         total=$((total+1))
                                 fi
                         fi
                 done
 
-		if [ "$file" != "${files[$i]}"$extention ];
-	        then
-        		echo " "
-                	echo -e "${green}[+] ${total} Unique Ipv4 segments."
-			sleep 1
-                        echo " "
-                       	echo -e "${green}[+] FILE SAVED IN ${red}====>${green} [ ${new_directory}/new-${files[$i]}-${code_file}${extention2} ]${white}"
-
-		elif [ "$file" == "${files[$i]}"$extention ];
+                if [ "$file" != "${files[$i]}"$extention ];
                 then
-			echo " "
-                        echo -e "${green}[+] OMITTED.${white}"
-		fi
+                        echo " "
+                        echo -e "${green} [+] ${total} Unique Ipv4 segments."
+                        echo " "
+                        sleep 1
+
+                        if [ "$total" != 0 ];
+                        then
+                                echo -e "${green} [+] FILE SAVED IN ${red}====>${green} [ ${new_directory}/new-${files[$i]}-${code_file}${extention2} ]${white}"
+                        else
+                                echo -e "${red} [+] NO Unique Ipv4 segments."
+                        fi
+
+                elif [ "$file" == "${files[$i]}"$extention ];
+                then
+                        echo " "
+                        echo -e "${red} [+] OMITTED.${white}"
+                fi
 
                 sleep 1
                 flag=0
@@ -144,56 +163,55 @@ function uniqs() {
 }
 
 #MAIN PROGRAM
+banner
 echo " "
-echo -e "${yellow}Este script fue elaborado con el proposito de comparar listas de segmentos IPv4 de un sensor de Global Cybersec.${white}"
-echo " "
-echo " "
-echo -e "${blue}-------------------------------------------${white}"
-echo -e "${blue} AVAILABLE FILES: ${#files[@]}${white}"
-echo -e "${blue}-------------------------------------------${white}"
+echo -e "${yellow} Este script fue elaborado con el proposito de comparar listas de segmentos IPv4 de un sensor de Global Cybersec.${white}"
 echo " "
 echo " "
-
-#SHOW AVAILABLE .TXT FILES IN THE WORK DIRECTORY
-echo -e "${blue}$(ls -l *.txt | awk '{print $9}') ${white}"
+echo -e "${blue} ------------------------${white}"
+echo -e "${blue} | AVAILABLE FILES: ${#files[@]}   |${white}"
+echo -e "${blue} ------------------------${white}"
+echo " "
 
 for i in "${files[@]}"
 do
-	if [[ ! -f ./"$i"$extention ]];
-	then
-		echo " "
-		echo -e "${red}ERROR, one of the files does not exist, validate that they are available.${white}"
-		check=1
-		exit
-	fi
+        if [[ ! -f ./"$i"$extention ]];
+        then
+                echo " "
+                echo -e "${red} ERROR, one of the files does not exist, validate that they are available.${white}"
+                check=1
+                exit
 
+        elif [[ -f ./"$i"$extention ]];
+        then
+                echo -e "${red} [+] $i$extention ${white}"
+        fi
 done
 
 if [[ "$check" -eq 0 && ! -d ./$new_directory ]];
 then
-	mkdir $new_directory
+        mkdir $new_directory
 fi
 
 while [ "$flag" -eq 1 ]
 do
-	while [[ "$attemps" -gt 0 && "$flag" -eq 1 ]]
+        while [[ "$attemps" -gt 0 && "$flag" -eq 1 ]]
         do
-        	echo " "
                 echo " "
-	 	#SELECT A TEXT FILE FROM THE OPTIONS DISPLAYED
-                echo -e "${gray}Enter a filename:${gray} "
+                #SELECT A TEXT FILE FROM THE OPTIONS DISPLAYED
+                echo -e "${gray} Enter a filename:${gray} "
                 read file
 
                 if [[ -f ./"${file}" ]];
                 then
-                      	echo " "
                         echo " "
-                        echo -e "${green}[+] File selected ====> [ ${file} ]${white}"
+                        echo " "
+                        echo -e "${green} [+] File selected ====> [ ${file} ]${white}"
                         echo " "
                         sleep 1
 
-			#START PROCESS
-                        echo -e "${green}[+] Analyzing file '${file}'....${white}"
+                        #START PROCESS
+                        echo -e "${green} [+] Analyzing file '${file}'....${white}"
                         echo " "
                         sleep 1
 
@@ -205,45 +223,45 @@ do
 
                         echo " "
                         echo " "
-                        echo -e "${green}[+] ENDED PROCESS.${white}"
+                        echo -e "${green} [+] ENDED PROCESS.${white}"
                         sleep 1
-		else
+                else
                         echo " "
                         echo " "
-                        echo -e "${red}ERROR, the specified filename is not correct, verify if the filename is writted correct!${white}"
+                        echo -e "${red} ERROR, the specified filename is not correct, verify if the filename is writted correct!${white}"
                         attemps=$((attemps-1))
 
                         if [ "$attemps" == 0 ];
                         then
-                        	echo " "
                                 echo " "
-                                echo "${red}ERROR, your attempts are exhausted, please try again later.${white}"
+                                echo " "
+                                echo "${red} ERROR, your attempts are exhausted, please try again later.${white}"
                                 sleep 1
                         fi
                 fi
-	done
+        done
 
         if [ "$flag" -eq 0 ];
         then
-        	echo " "
                 echo " "
-		#AT THE END OF A PROCESS, INDICATE Y IF YOU WANT TO ANALYZE ANOTHER FILE, OR N IF YOU WANT TO EXIT.
-                echo -e "${gray}I you need analyze other file? (s/n):${white} "
+                echo " "
+                #AT THE END OF A PROCESS, INDICATE Y IF YOU WANT TO ANALYZE ANOTHER FILE, OR N IF YOU WANT TO EXIT.
+                echo -e "${gray} I you need analyze other file? (s/n):${white} "
                 read repeat
 
                 if [[ "$repeat" == "n" || "$repeat" == "N" ]];
                 then
-                	echo "EXITING..."
+                        echo "EXITING..."
                         flag=0
-			sleep 1
-			clear
+                        sleep 1
+                        clear
 
                 elif [[ "$repeat" == "s" || "$repeat" == "S" ]];
                 then
-	                flag=1
+                        flag=1
                         attemps=3
                 else
-                        echo -e "${red}ERROR, invalid option.${white}"
+                        echo -e "${red} ERROR, invalid option.${white}"
                 fi
-	fi
+        fi
 done
